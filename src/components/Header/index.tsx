94bedbb31cd7455a8027e4797f1b8979efbbec9a
Angelo Filho <angelo.filho06@hotmail.com>
2023-05-12 18:28:40 -0300

    feat: add responsivity to the header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,48 @@
-import Link from "next/link";
+import { List } from "phosphor-react";
 import { ActiveLink } from "../ActiveLink";
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
+import Image from "next/image";
+
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger asChild>
+            <button>
+              <List size={24} />
+            </button>
+          </DropdownMenu.Trigger>
 
-        <nav>
-          <ActiveLink activeClassName={styles.active} href="/">
-            <a>Home</a>
-          </ActiveLink>
-          <ActiveLink activeClassName={styles.active} href="/posts">
-            <a>Posts</a>
-          </ActiveLink>
-        </nav>
+          <DropdownMenu.Content asChild sideOffset={16}>
+            <nav className={styles.mobileMenu}>
+              <ActiveLink activeClassName={styles.active} href="/">
+                <a>Home</a>
+              </ActiveLink>
+              <ActiveLink activeClassName={styles.active} href="/posts">
+                <a>Posts</a>
+              </ActiveLink>
+            </nav>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
 
-        <SignInButton />
+        <Image src="/images/logo.svg" alt="ig.news" width={110} height={51} />
+
+        <div className={styles.rightContent}>
+          <nav>
+            <ActiveLink activeClassName={styles.active} href="/">
+              <a>Home</a>
+            </ActiveLink>
+            <ActiveLink activeClassName={styles.active} href="/posts">
+              <a>Posts</a>
+            </ActiveLink>
+          </nav>
+
+          <SignInButton />
+        </div>
       </div>
     </header>
   );
